ee630a12e87f26eef41f26a0e56084fb6d97a195
Luke <neumalu1@fel.cvut.cz>
2024-11-05 08:44:03 +0100

    Added Kaiming init

--- rci_scripts/VKDNW_EVOLUTION_FLOPS480000000_LAYERS14_PARAMS_RANDOM_FIXED_ImageNet_50ep_kaiming.sh
+++ rci_scripts/VKDNW_EVOLUTION_FLOPS480000000_LAYERS14_PARAMS_RANDOM_FIXED_ImageNet_50ep_kaiming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=VKDNW_EVOLUTION_FLOPS480000000_LAYERS14_PARAMS_RANDOM_FIXED
 #SBATCH --time=24:00:00
-#SBATCH --partition=amdgpulong
+#SBATCH --partition=amdgpu
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128gb
