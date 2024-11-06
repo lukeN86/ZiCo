1f1d1a86c877e3a152600a97a5ae943b94f66802
Luke <neumalu1@fel.cvut.cz>
2024-11-06 12:59:12 +0100

    Larger models

--- rci_scripts/VKDNW_EVOLUTION_FLOPS_1050M_17L_ImageNet_50ep_kaiming.sh
+++ rci_scripts/VKDNW_EVOLUTION_FLOPS_1050M_17L_ImageNet_50ep_kaiming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=VKDNW_EVOLUTION_FLOPS_1050M_17L
 #SBATCH --time=24:00:00
-#SBATCH --partition=amdgpulong
+#SBATCH --partition=amdgpudeadline
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128gb
