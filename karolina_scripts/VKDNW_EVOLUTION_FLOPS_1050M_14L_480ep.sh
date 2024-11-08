06bd9ef527849d5cb027f2e66e401c248916cd35
Luke <neumalu1@fel.cvut.cz>
2024-11-08 07:43:03 +0100

    Larger models

--- karolina_scripts/VKDNW_EVOLUTION_FLOPS_1050M_14L_480ep.sh
+++ karolina_scripts/VKDNW_EVOLUTION_FLOPS_1050M_14L_480ep.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name VKDNW_EVOLUTION_FLOPS_1050M_14L_480ep
-#SBATCH --account OPEN-30-33
-#SBATCH --partition qdgx
+#SBATCH --job-name VKDNW_EVOLUTION_FLOPS_1050M_14L
+#SBATCH --account FTA-24-40
+#SBATCH --partition qgpu
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 64
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus 8
 #SBATCH --time 48:00:00
 #SBATCH --output=/home/neumalu/jobs/zico_%j.out
 # -------------------------------
-ml GCC/10.3.0
-ml CMake/3.14.1
-ml CUDA
+ml fosscuda/2020b
+ml Anaconda3
 
-eval "$(/home/neumalu/anaconda3/bin/conda shell.bash hook)"
 
-conda activate zico
-export OMP_NUM_THREADS=14
+source activate /mnt/proj2/open-30-33/conda/zico
 
-save_dir=/mnt/proj2/open-30-33/zico/VKDNW_EVOLUTION_FLOPS_1050M_14L_480ep
+
+save_dir=/mnt/proj2/open-30-33/zico_karolina/VKDNW_EVOLUTION_FLOPS_1050M_14L_480ep
 mkdir -p ${save_dir}
 
 resolution=224
 epochs=480
 
 
-horovodrun -np 16 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
+horovodrun -np 8 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
   --dist_mode single --workers_per_gpu 8 \
   --input_image_size ${resolution} --epochs ${epochs} --warmup 5 \
   --optimizer sgd --bn_momentum 0.01 --wd 4e-5 --nesterov --weight_init custom \
@@ -31,7 +31,7 @@ horovodrun -np 16 python ts_train_image_classification.py --dataset imagenet --n
   --lr_per_256 0.1 --target_lr_per_256 0.0 --lr_mode cosine \
   --arch Masternet.py:MasterNet \
   --plainnet_struct_txt ZiCo/VKDNW_EVOLUTION_FLOPS_1050M_14L.txt \
-  --teacher_arch geffnet_tf_efficientnet_b5_ns \
+  --teacher_arch geffnet_tf_efficientnet_b3_ns \
   --teacher_pretrained \
   --teacher_input_image_size 320 \
   --teacher_feature_weight 1.0 \
