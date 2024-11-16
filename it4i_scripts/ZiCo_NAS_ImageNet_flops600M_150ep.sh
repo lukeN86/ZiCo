6088fdf9621c3d30cc1c5b5680d03b43468f8523
Luke <neumalu1@fel.cvut.cz>
2024-11-16 12:49:34 +0100

    Larger models

---
+++ it4i_scripts/ZiCo_NAS_ImageNet_flops600M_150ep.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name ZiCo600M
+#SBATCH --account OPEN-30-33
+#SBATCH --partition qdgx
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 64
+#SBATCH --time 24:00:00
+#SBATCH --output=/home/neumalu/jobs/zico_%j.out
+# -------------------------------
+ml GCC/10.3.0
+ml CMake/3.14.1
+ml CUDA
+
+eval "$(/home/neumalu/anaconda3/bin/conda shell.bash hook)"
+
+conda activate zico
+export OMP_NUM_THREADS=14
+
+save_dir=/mnt/proj2/open-30-33/zico/ZiCo_NAS_ImageNet_flops450M_150ep
+mkdir -p ${save_dir}
+
+resolution=224
+epochs=150
+
+
+horovodrun -np 16 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
+  --dist_mode single --workers_per_gpu 8 \
+  --input_image_size ${resolution} --epochs ${epochs} --warmup 5 \
+  --optimizer sgd --bn_momentum 0.01 --wd 4e-5 --nesterov --weight_init custom \
+  --label_smoothing --random_erase --mixup --auto_augment \
+  --lr_per_256 0.1 --target_lr_per_256 0.0 --lr_mode cosine \
+  --arch Masternet.py:MasterNet \
+  --plainnet_struct_txt ZiCo/ZiCo_imagenet1k_flops600M_res224.txt \
+  --teacher_arch geffnet_tf_efficientnet_b3_ns \
+  --teacher_pretrained \
+  --teacher_input_image_size 320 \
+  --teacher_feature_weight 1.0 \
+  --teacher_logit_weight 1.0 \
+  --ts_proj_no_relu \
+  --ts_proj_no_bn \
+  --use_se \
+  --target_downsample_ratio 16 \
+  --batch_size_per_gpu 64 --save_dir ${save_dir} \
+  --world-size 1 \
+  --dist_mode horovod
+  
+
+
+
